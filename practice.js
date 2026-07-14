22d671dfa33856c4174ca5aade1ba37fbf3453ef
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-14 13:34:17 +0545

    NodeJS HTTP/2

--- practice.js
+++ practice.js
@@ -1,6 +1,26 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    console.log(req.method); // ---> GET,PUT,POST,DELETE
-    console.log(req.url); // ---> /users , /products ,/login
-    console.log(req.headers); // ---> {authorization:'bearer token...'}
-});
+// const http = require('http');
+// const server = http.createServer((req,res)=>{
+//     console.log(req.method); // ---> GET,PUT,POST,DELETE
+//     console.log(req.url); // ---> /users , /products ,/login
+//     console.log(req.headers); // ---> {authorization:'bearer token...'}
+// });
+
+
+// let http2;
+// try{
+//     http2 = require('node:http2');
+// }
+// catch(err){
+//     console.log('http support is disabled!');
+// }
+
+// console.log(1===1);
+
+// let http2;
+// try {
+//   http2 = await import('node:http2');
+// } catch (err) {
+//   console.error('http2 support is disabled!');
+// }
+
+
